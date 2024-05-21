a52ee30f657d66c261339eb0718bb392bd830182
박지영 <jypark@monkey-soft.com>
2024-05-22 08:36:00 +0900

    섹션 컴포넌트 구조화

---
+++ src/components/section/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div id='header' role='banner'>
+      Header
+    </div>
+  )
+}
+
+export default Header
---
+++ src/components/section/Main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Main = () => {
+  return (
+    <div id='main' role='main'>
+      Main
+    </div>
+  )
+}
+
+export default Main
