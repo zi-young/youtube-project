6413884682b4210b4a75187df3516fcf68190c18
박지영 <jypark912@monkey-soft.com>
2024-05-27 08:26:07 +0900

    컴포넌트 비동기 작업

--- src/components/section/Main.jsx
+++ src/components/section/Main.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import Header from './Header'
+import Footer from './Footer'
 
-const Main = () => {
+const Main = (props) => {
   return (
-    <div id='main' role='main'>
-      Main
-    </div>
+    <>
+      <Header />
+        <main id='main' role='main'>
+          {props.children}
+        </main>
+      <Footer />
+    </>
   )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Main from '../components/section/Main'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <Main>
+		Home
+	</Main>
   )
 }
 
