f185942ae5bc65f45c67c9ca6ac343d2b43e5535
박지영 <jypark912@monkey-soft.com>
2024-05-30 08:56:56 +0900

    추천 영상 작업

--- src/components/contents/Today.jsx
+++ src/components/contents/Today.jsx
@@ -1,8 +1,35 @@
 import React from 'react'
+import { todayText } from '../../data/today'
+import { Link } from 'react-router-dom'
 
 const Today = () => {
   return (
-    <div>Today</div>
+    <section id='today'>
+		<div className="today__inner">
+			<div className="today__thumb play__icon">
+				<Link to={todayText[0].page}>
+					<img src={todayText[0].img} alt={todayText[0].title} />
+				</Link>
+			</div>
+			<div className="today__text">
+				<span className='today'>today!</span>
+				<h3 className='title'>
+					<Link to= {todayText[0].page}>
+						{todayText[0].title}
+					</Link>
+				</h3>
+				<p className='desc'>{todayText[0].desc}</p>
+				<div className='info'>
+					<span className='author'>
+						<Link to={`/channel/${todayText[0].channelId}`}>
+							{todayText[0].author}
+						</Link>
+					</span>
+					<span className='date'>{todayText[0].date}</span>
+				</div>
+			</div>
+		</div>
+    </section>
   )
 }
 
--- src/pages/Today.jsx
+++ src/pages/Today.jsx
@@ -1,12 +1,45 @@
 import React from 'react'
 import Main from '../components/section/Main'
+import { todayText } from '../data/today'
+import { Link } from 'react-router-dom'
+
 
 const Today = () => {
   return (
     <Main 
         title = "추천 영상"
         description="오늘의 추천 유튜브 영상입니다.">
-        Today
+        
+        <section id='todayPage'>
+          <h2>오늘의 추천 영상입니다.</h2>
+
+          {todayText.map((today, key) => (
+			<div className="today__inner play__icon" key={key}>
+				<div className="today__thumb">
+					<Link to={today.page}>
+						<img src={today.img} alt={today.title} />
+					</Link>
+				</div>
+				<div className="today__text">
+					<span className='today'>today!</span>
+					<h3 className='title'>
+						<Link to= {today.page}>
+							{today.title}
+						</Link>
+					</h3>
+					<p className='desc'>{today.desc}</p>
+					<div className='info'>
+						<span className='author'>
+							<Link to={`/channel/${today.channelId}`}>
+								{today.author}
+							</Link>
+						</span>
+						<span className='date'>{today.date}</span>
+					</div>
+				</div>
+			</div>
+          ))}
+        </section>
     </Main>
   )
 }
