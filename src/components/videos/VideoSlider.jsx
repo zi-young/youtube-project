f4171ff27af65a80d51246482105e02f2165c1c5
jypark <jypark912@monkey-soft.com>
2024-06-18 15:49:02 +0900

    비디오 컴포넌트 통합 작업

---
+++ src/components/videos/VideoSlider.jsx
@@ -0,0 +1,68 @@
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/navigation';
+
+import { Navigation } from 'swiper/modules';
+
+const VideoSlider = ({ videos, title, id }) => {
+    const [loading, setLoading] = useState(true); 
+
+    useEffect(() => {
+        setTimeout(() => {
+            setLoading(false);
+        }, 500);
+    }, []);
+
+    const youtubeClass = loading ? 'isLoading' : 'isLoaded';
+
+    return (
+        <section id={id} className={youtubeClass}>
+            <h2>{title}</h2>
+
+            <div className='video__slider'>
+                <Swiper
+                    slidesPerView={1}
+                    spaceBetween={20}
+                    navigation={true}
+                    modules={[Navigation]}
+                    className={`mySwiper-${id}`}
+                    breakpoints={{
+                        640: {
+                            slidesPerView: 2,
+                            spaceBetween: 20,
+                        },
+                        768: {
+                            slidesPerView: 3,
+                            spaceBetween: 20,
+                        },
+                        1024: {
+                            slidesPerView: 4,
+                            spaceBetween: 20,
+                        },
+                        1600: {
+                            slidesPerView: 4,
+                            spaceBetween: 20,
+                        },
+                    }}
+                >
+                    {videos.map((video, key) => (
+                        <SwiperSlide key={key}>
+                            <div className="video" key={key}>
+                                <div className="video__thumb play__icon">
+                                    <Link to={`/video/${video.videoId}`}>
+                                        <img src={video.img} alt={video.title} />
+                                    </Link>
+                                </div>
+                            </div>
+                        </SwiperSlide>
+                    ))}
+                </Swiper>
+            </div>
+        </section>
+    )
+}
+
+export default VideoSlider
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,9 +2,13 @@ import React from 'react'
 import Main from '../components/section/Main'
 import Today from '../components/contents/Today'
 import Developer from '../components/contents/Developer'
-import VideoSlider from '../components/contents/videos'
+import VideoSlider from '../components/videos/VideoSlider'
 
 import {webdText} from '../data/webd'
+import {websiteText} from '../data/website'
+import {gsapText} from '../data/gsap'
+import {portfolioText} from '../data/portfolio'
+import {youtubeText} from '../data/youtube'
 
 
 const Home = () => {
@@ -16,7 +20,10 @@ const Home = () => {
 			<Today />
 			<Developer />
 			<VideoSlider videos={webdText} title="😮 웹디자인기능사 준비는 이걸로!" id="wedb" />
-
+			<VideoSlider videos={websiteText} title="😍 웹표준 사이트 만들기" id="wedsite" />
+			<VideoSlider videos={gsapText} title="🤣 GSAP 패럴랙스 효과를 하고 싶다면" id="gasp" />
+			<VideoSlider videos={portfolioText} title="😁 포트폴리오 만드는 방법을 공유합니다." id="portfolio" />
+			<VideoSlider videos={youtubeText} title="👍 지금 이코딩을 영상으로" id="youtube" />
 		</Main>
   )
 }
